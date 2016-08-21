6579b8194c64b1717f5e598922b9d38f9c823676
KemalC <ddeveloperr@gmail.com>
2016-08-21 20:28:15 +0000

    d2 - Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,33 @@
 class Product < ActiveRecord::Base
-    ## The field’s title, description, and image URL are not empty.
-    validates :title, :description, :image_url, presence: true
-    ## The price is a valid number not less than $0.01.
-    validates :price, numericality: {greater_than_or_equal_to: 0.01}
-    ## The title is unique among all products.
-    validates :title, uniqueness: true
-    ## The image URL looks reasonable
-    validates :image_url, allow_blank: true, format: {
-      with: %r{\.(gif|jpg|png)\Z}i,
-      message: 'must be a URL for GIF, JPG or PNG image.'
-    }
-    
-    def self.latest
-      Product.order(:updated_at).last
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # 
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :title, length: {minimum: 10}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
     end
     
 end
